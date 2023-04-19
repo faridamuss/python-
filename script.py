d25f320c296906d05fb13a1b480461198325affd
Farida MG <faridamus@protonmail.com>
2023-04-18 20:09:29 -0400

    working with lists

--- script.py
+++ script.py
@@ -15,3 +15,7 @@ desc = "compound interest"
 
 # Create a variable profitable
 profitable = True 
+
+#Creating list (arrays)
+house = ["hallway", 11.25, "kitchen", 18.0, "living room", 20.0, "bedroom", 10.75, "bathroom", 9.50]
+
