21c6a5b827ccd84f596593bb0e3368a95fb0dabb
Farida MG <faridamus@protonmail.com>
2023-11-05 11:29:27 -0500

    working with input() function

--- variables.py
+++ variables.py
@@ -1,33 +1,33 @@
 # "I'd like a large pizza with feta cheese, prosciutto, olives, and arugula, on a gluten-free crust. Please deliver it to 401 Somewhere St. Specifically, to the treehouse in the back yard."
 
-pizza = {
-    'size': 'large',
-    'toppings': [
-        'feta',
-        'prosciutto',
-        'olives',
-        'arugula'
-    ],
-    'crust': 'gluten free',
-    'address': '401 Somewhere St.',
-    'note': 'Deliver to treehouse in back yard.'
-}
-print(pizza)
+# pizza = {
+#     'size': 'large',
+#     'toppings': [
+#         'feta',
+#         'prosciutto',
+#         'olives',
+#         'arugula'
+#     ],
+#     'crust': 'gluten free',
+#     'address': '401 Somewhere St.',
+#     'note': 'Deliver to treehouse in back yard.'
+# }
+# print(pizza)
 
 # name is a variable. It contains a string
-name = "Thomas"
+name = input()
 print("Hello " + name + ", it's very nice to meet you!")
 
 # taxRate, subtotal, tax and total are all variables that contain numbers
-taxRate = 0.14
-subtotal = 20
-tax = subtotal * taxRate
-total = subtotal + tax
-print("Your total bill is: ")
-print(total)
+# taxRate = 0.14
+# subtotal = 20
+# tax = subtotal * taxRate
+# total = subtotal + tax
+# print("Your total bill is: ")
+# print(total)
 
 # travelDestinations is a variable containing a list
 # city is a variable - its value is changing a few times
-travelDestinations = ["Vancouver","Montreal","Calgary","Toronto"]
-for city in travelDestinations:
-    print(city + " looks like a cool place to go!!")
+# travelDestinations = ["Vancouver","Montreal","Calgary","Toronto"]
+# for city in travelDestinations:
+#     print(city + " looks like a cool place to go!!")
