0873618e3c90502eedb0b470d7423d37fb33f933
Farida MG <faridamus@protonmail.com>
2023-11-05 18:47:40 -0500

    working with dictionary data type

---
+++ dictionaries.py
@@ -0,0 +1,9 @@
+# A dictionary consists of a collection of key/value pairs. Each key-value pair maps the key to its associated value. You can define a dictionary by ecolosing a comma-separated list of key-value pairs in curly braces {}. A colon (:) separated list of key-value pairs:
+
+MLB_team = {
+   "Colorado": "Rockies",
+   "Boston": "Red Sox", 
+   "Minnesota": "Twins",
+}
+
+print(MLB_team['Minnesota']) # you can access a value in the dictionary by its key name
