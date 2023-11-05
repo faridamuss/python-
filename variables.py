643d361af01db6a9ea448fd8aec6e9bf50d8f402
Farida MG <faridamus@protonmail.com>
2023-11-05 11:14:14 -0500

    Working with lists

--- variables.py
+++ variables.py
@@ -13,3 +13,21 @@ pizza = {
     'note': 'Deliver to treehouse in back yard.'
 }
 print(pizza)
+
+# name is a variable. It contains a string
+name = "Thomas"
+print("Hello " + name + ", it's very nice to meet you!")
+
+# taxRate, subtotal, tax and total are all variables that contain numbers
+taxRate = 0.14
+subtotal = 20
+tax = subtotal * taxRate
+total = subtotal + tax
+print("Your total bill is: ")
+print(total)
+
+# travelDestinations is a variable containing a list
+# city is a variable - its value is changing a few times
+travelDestinations = ["Vancouver","Montreal","Calgary","Toronto"]
+for city in travelDestinations:
+    print(city + " looks like a cool place to go!!")
