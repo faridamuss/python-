02d87b8a9505fa7c4fc2370c2e8554fb41ad8c05
Farida MG <faridamus@protonmail.com>
2023-11-05 12:25:19 -0500

    Numbers in python

---
+++ numbers.py
@@ -0,0 +1,18 @@
+# x = 3
+# x = x + 1
+# print(x)
+
+# To increment means to increase and to decrement means to decrease. Most coders don't write x = x+ 1 as they prefer the shorter notation, x += 1, which means the same thing.
+
+# Let's consider the following task: ask a user to input any integer and decrease its value by 5. Try this out in your VSCode:
+# anyNum = int(input())
+# anyNum -= 5
+# print(anyNum)
+
+tripleUp = 5
+tripleUp *= 3
+print(tripleUp) # 15
+
+quarterValue = 100
+quarterValue /= 4
+print(quarterValue) # 25.0
