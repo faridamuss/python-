7bb556db5b74e1dde1c5552ad824efa7980a7d78
Farida MG <faridamus@protonmail.com>
2023-11-05 18:36:45 -0500

    accessing lists by index

--- lists.py
+++ lists.py
@@ -1,3 +1,8 @@
 # Python lists is a collection of arbitrary objects, somewhat akin to an array in many other languages but more flexible. Lists are defined in Python by enclosing a comma-separated sequence of objects in square brackets [], as shown below: 
+# a = ['foo', 'bar', 'baz', 'qux']
+# print(a)
+
+# Lists can be accessed by index 
 a = ['foo', 'bar', 'baz', 'qux']
-print(a)
+print(a[3])
+print(a[1]) 
