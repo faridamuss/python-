b8ac2b41ea6b0d7762a091e610ffb3c3dc693c78
Farida MG <faridamus@protonmail.com>
2023-11-05 11:06:33 -0500

    Working with data types

---
+++ variables.py
@@ -0,0 +1,15 @@
+# "I'd like a large pizza with feta cheese, prosciutto, olives, and arugula, on a gluten-free crust. Please deliver it to 401 Somewhere St. Specifically, to the treehouse in the back yard."
+
+pizza = {
+    'size': 'large',
+    'toppings': [
+        'feta',
+        'prosciutto',
+        'olives',
+        'arugula'
+    ],
+    'crust': 'gluten free',
+    'address': '401 Somewhere St.',
+    'note': 'Deliver to treehouse in back yard.'
+}
+print(pizza)
