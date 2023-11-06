5e11b3fdf2e55759af412e121b7abdbf7cecc7e4
Farida MG <faridamus@protonmail.com>
2023-11-05 19:17:28 -0500

    practicing with variable

---
+++ task.py
@@ -0,0 +1,15 @@
+# num1 = 2
+# num2 = 2.3
+# total = num1 + num2
+# print(type(total))
+
+# height = 170
+# mass = 56
+# bmi = mass/(height * height)
+# print(bmi)
+
+name = "Maria"
+surname = "Garcia"
+fullname = name + " " + surname 
+
+print((surname + " ") * 10)
